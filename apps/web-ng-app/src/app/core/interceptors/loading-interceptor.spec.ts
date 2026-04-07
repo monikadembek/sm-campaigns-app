a4793aaa739685f28b6dc1f531a44aacc1ae13b2
Monika <monika.dembek@gmail.com>
2026-04-07 19:57:55 +0200

    Add unit tests for loading service and interceptor

--- apps/web-ng-app/src/app/core/interceptors/loading-interceptor.spec.ts
+++ apps/web-ng-app/src/app/core/interceptors/loading-interceptor.spec.ts
@@ -1,17 +1,124 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpContext,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { of, Subject } from 'rxjs';
 
-import { loadingInterceptor } from './loading-interceptor-interceptor';
+import { loadingInterceptor } from './loading-interceptor';
+import { LoadingService } from '../services/loading.service';
+import { SkipLoadingToken } from './skip-loading-token';
 
 describe('loadingInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) =>
+  let loadingService: LoadingService;
+
+  const runInterceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn) =>
     TestBed.runInInjectionContext(() => loadingInterceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    loadingService = TestBed.inject(LoadingService);
   });
 
   it('should be created', () => {
+    const interceptor: HttpInterceptorFn = (req, next) =>
+      TestBed.runInInjectionContext(() => loadingInterceptor(req, next));
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should call loadingOn when a request starts', () => {
+    const req = new HttpRequest('GET', '/api/test');
+    const next: HttpHandlerFn = () => of(new HttpResponse());
+
+    vi.spyOn(loadingService, 'loadingOn');
+
+    runInterceptor(req, next).subscribe();
+
+    expect(loadingService.loadingOn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call loadingOff when a request completes', () => {
+    const req = new HttpRequest('GET', '/api/test');
+    const next: HttpHandlerFn = () => of(new HttpResponse());
+
+    vi.spyOn(loadingService, 'loadingOff');
+
+    runInterceptor(req, next).subscribe();
+
+    expect(loadingService.loadingOff).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call loadingOff when a request errors', () => {
+    const req = new HttpRequest('GET', '/api/test');
+    const next: HttpHandlerFn = () => {
+      const subject = new Subject<HttpResponse<unknown>>();
+      // Emit error asynchronously so we can subscribe first
+      Promise.resolve().then(() => subject.error(new Error('network error')));
+      return subject.asObservable();
+    };
+
+    vi.spyOn(loadingService, 'loadingOff');
+
+    runInterceptor(req, next).subscribe({
+      error: () => {
+        // expected
+      },
+    });
+
+    // finalize runs synchronously on error for Subject
+    // but we used Promise.resolve, so we need to wait
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        expect(loadingService.loadingOff).toHaveBeenCalledTimes(1);
+        resolve();
+      });
+    });
+  });
+
+  it('should skip loading when SkipLoadingToken is true', () => {
+    const context = new HttpContext().set(SkipLoadingToken, true);
+    const req = new HttpRequest('GET', '/api/test', { context });
+    const mockResponse = new HttpResponse({ body: 'skipped' });
+    const next: HttpHandlerFn = () => of(mockResponse);
+
+    vi.spyOn(loadingService, 'loadingOn');
+    vi.spyOn(loadingService, 'loadingOff');
+
+    let response: HttpResponse<unknown> | undefined;
+    runInterceptor(req, next).subscribe((res) => {
+      response = res as HttpResponse<unknown>;
+    });
+
+    expect(loadingService.loadingOn).not.toHaveBeenCalled();
+    expect(loadingService.loadingOff).not.toHaveBeenCalled();
+    expect(response).toBe(mockResponse);
+  });
+
+  it('should not skip loading when SkipLoadingToken is false (default)', () => {
+    const req = new HttpRequest('GET', '/api/test');
+    const next: HttpHandlerFn = () => of(new HttpResponse());
+
+    vi.spyOn(loadingService, 'loadingOn');
+
+    runInterceptor(req, next).subscribe();
+
+    expect(loadingService.loadingOn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should pass the request through to the next handler', () => {
+    const req = new HttpRequest('GET', '/api/test');
+    const mockResponse = new HttpResponse({ body: 'data' });
+    const next = vi.fn<HttpHandlerFn>(() => of(mockResponse));
+
+    let response: HttpResponse<unknown> | undefined;
+    runInterceptor(req, next).subscribe((res) => {
+      response = res as HttpResponse<unknown>;
+    });
+
+    expect(next).toHaveBeenCalledWith(req);
+    expect(response).toBe(mockResponse);
   });
 });
--- apps/web-ng-app/src/app/core/services/loading.service.spec.ts
+++ apps/web-ng-app/src/app/core/services/loading.service.spec.ts
@@ -13,4 +13,46 @@ describe('LoadingService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should initially have isLoading as false', () => {
+    expect(service.isLoading()).toBe(false);
+  });
+
+  it('should set isLoading to true when loadingOn is called', () => {
+    service.loadingOn();
+    expect(service.isLoading()).toBe(true);
+  });
+
+  it('should set isLoading back to false when loadingOff is called after loadingOn', () => {
+    service.loadingOn();
+    service.loadingOff();
+    expect(service.isLoading()).toBe(false);
+  });
+
+  it('should remain loading when multiple requests are active', () => {
+    service.loadingOn();
+    service.loadingOn();
+    service.loadingOff();
+    expect(service.isLoading()).toBe(true);
+  });
+
+  it('should stop loading only when all concurrent requests finish', () => {
+    service.loadingOn();
+    service.loadingOn();
+    service.loadingOn();
+
+    service.loadingOff();
+    expect(service.isLoading()).toBe(true);
+
+    service.loadingOff();
+    expect(service.isLoading()).toBe(true);
+
+    service.loadingOff();
+    expect(service.isLoading()).toBe(false);
+  });
+
+  it('should allow counter to go below zero if loadingOff is called without loadingOn', () => {
+    service.loadingOff();
+    expect(service.isLoading()).toBe(false);
+  });
 });
